9b37586f6e5be8fd0cd65eaa3aee450a8f7df3f6
elie987654321 <elie987654321@gmail.com>
2023-04-05 16:04:48 -0400

    Update GameGraphicalController.java

--- FormatifMVC/src/controller/GameGraphicalController.java
+++ FormatifMVC/src/controller/GameGraphicalController.java
@@ -66,27 +66,63 @@ public class GameGraphicalController{
     private Text tour;
 
     @FXML
+    private void handleButtonClickCDemander(ActionEvent event) {
+        System.out.println("demander");
+    }    @FXML
     private void handleButtonClickConserver(ActionEvent event) {
         System.out.println("conserver");
 
     }
 
     @FXML
-    private void handleButtonClickCDemander(ActionEvent event) {
+    private void handleButtonClickCDemanderJoueur2(ActionEvent event) {
         System.out.println("demander");
+    }    @FXML
+    private void handleButtonClickConserverJoueur2(ActionEvent event) {
+        System.out.println("conserver");
+
     }
 
 
+    @FXML
+    private void handleButtonClickConserverJOueur3(ActionEvent event) {
+        System.out.println("conserver");
+
+    }
+
+    @FXML
+    private void handleButtonClickDemanderJoueur4(ActionEvent event) {
+        System.out.println("demander");
+    }
+
+    private void InitializerPartie()
+    {
+        
+
+        pointJoueur1.setText(String.valueOf(partie.getJoueur1().getPoints()));
+        carteJoueur1.setText(partie.getJoueur1().getMainDu21Bien());
+        conserverJoueur1.setOnAction(this::handleButtonClickConserver);
+        demanderJoueur1.setOnAction(this::handleButtonClickCDemander);
+        pointJoueur2.setText(String.valueOf(partie.getJoueur1().getPoints()));
+        carteJoueur2.setText(partie.getJoueur1().getMainDu21Bien());
+        conserverJoueur2.setOnAction(this::handleButtonClickConserver);
+        demanderJoueur2.setOnAction(this::handleButtonClickCDemander);
+        pointJoueur3.setText(String.valueOf(partie.getJoueur1().getPoints()));
+        carteJoueur3.setText(partie.getJoueur1().getMainDu21Bien());
+        conserverJoueur3.setOnAction(this::handleButtonClickConserver);
+        demanderJoueur3.setOnAction(this::handleButtonClickCDemander);
+        pointJoueur4.setText(String.valueOf(partie.getJoueur1().getPoints()));
+        carteJoueur4.setText(partie.getJoueur1().getMainDu21Bien());
+        conserverJoueur4.setOnAction(this::handleButtonClickConserver);
+        demanderJoueur4.setOnAction(this::handleButtonClickCDemander);
+
+        JoueurCreateur factory = new JoueurCreateur();
+        JoueurProduit joueur = factory.CreerJoueur(4);
+
+    }
 
     @FXML
     private void initialize() {
-        partie.DistribuerCartesInitials();
-        pointJoueur1.setText(String.valueOf(partie.getJoueur1().getPoints()));
-        carteJoueur1.setText(partie.getJoueur1().getMainDu21Bien());
-        conserverLeJeu.setOnAction(this::handleButtonClickConserver);
-        demanderUneCarte.setOnAction(this::handleButtonClickCDemander);
-        JoueurCreateur factory = new JoueurCreateur();
-        JoueurProduit joueur = factory.CreerJoueur(4);
 
         int i = 0;
     }
