8695e3cf7566dfcdfbde359fe583d223d7a4d202
AC-climoilou <1701633@cegeplimoilou.ca>
2023-04-12 16:20:53 -0400

    interface des noms et validations des noms

--- FormatifMVC/src/controller/ChoixDesNomsGraphicalController.java
+++ FormatifMVC/src/controller/ChoixDesNomsGraphicalController.java
@@ -22,7 +22,7 @@ public class ChoixDesNomsGraphicalController {
     private Button commencer;
 
     @FXML
-    private void handleButtonClickCommencer(ActionEvent event) throws Exception {
+    private void handleButtonClickCommencer(ActionEvent event) {
         try{
             Stage primaryStage = new Stage();
 
@@ -30,13 +30,45 @@ public class ChoixDesNomsGraphicalController {
 
             FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("/view/Game.fxml"));
             Scene scene = new Scene(fxmlLoader1.load());
+            GameGraphicalController gameGraphicalController = (GameGraphicalController) fxmlLoader1.getController();
 
-            primaryStage.setScene(scene);
-            primaryStage.show();
+            // Validation des noms
+            int i = 0;
+
+            if(nomJoueur1.getText() != "" && !nomJoueur1.getText().equals(nomJoueur2.getText()) && !nomJoueur1.getText().equals(nomJoueur3.getText()) && !nomJoueur1.getText().equals(nomJoueur4.getText())){
+                gameGraphicalController.joueur1.setText(nomJoueur1.getText());
+                i++;
+            }
+
+            if(nomJoueur2.getText() != "" && !nomJoueur2.getText().equals(nomJoueur1.getText()) && !nomJoueur2.getText().equals(nomJoueur3.getText()) && !nomJoueur2.getText().equals(nomJoueur4.getText())){
+                gameGraphicalController.joueur2.setText(nomJoueur2.getText());
+                i++;
+            }
+
+            if(nomJoueur3.getText() != "" && !nomJoueur3.getText().equals(nomJoueur1.getText()) && !nomJoueur3.getText().equals(nomJoueur2.getText()) && !nomJoueur3.getText().equals(nomJoueur4.getText())){
+                gameGraphicalController.joueur3.setText(nomJoueur3.getText());
+                i++;
+            }
+
+            if(nomJoueur4.getText() != "" && !nomJoueur4.getText().equals(nomJoueur1.getText()) && !nomJoueur4.getText().equals(nomJoueur2.getText()) && !nomJoueur4.getText().equals(nomJoueur3.getText())){
+                gameGraphicalController.joueur4.setText(nomJoueur4.getText());
+                i++;
+            }
+            System.out.println(nomJoueur1.getText());
+            System.out.println(nomJoueur2.getText());
+            System.out.println(nomJoueur3.getText());
+            System.out.println(nomJoueur4.getText());
+
+            System.out.println(i);
+            if(i==4){
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            }
 
         }catch (Exception e){
             e.printStackTrace();
         }
+
 
     }
 }
